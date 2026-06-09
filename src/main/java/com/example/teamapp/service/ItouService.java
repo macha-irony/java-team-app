8372e9f296de199d13102c667bbe8477e0ca90f4
3030691 <3030691@30306-91>
2026-06-09 10:00:28 +0900

    テスト：Serviceクラス（伊藤）作成しました！

---
+++ src/main/java/com/example/teamapp/service/ItouService.java
@@ -0,0 +1,9 @@
+package com.example.teamapp.service;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItouService {
+	public String getProfile() {
+        return "メンバーの伊藤です！趣味はイラストです。";
+    }
+}
