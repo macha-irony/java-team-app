9b24c9abd7d293acedeebe02cfdc4bfa27889756
3030718 <3030718@30307-18>
2026-06-09 09:56:15 +0900

    test yamamoto

---
+++ src/main/java/com/example/teamapp/service/YamamotoService.java
@@ -0,0 +1,13 @@
+package com.example.teamapp.service;
+
+
+
+	
+	import org.springframework.stereotype.Service;
+
+	@Service
+	public class YamamotoService {
+	    public String getProfile() {
+	        return "メンバーの山本です！趣味は趣味は昼寝です。";
+	    }
+	}
